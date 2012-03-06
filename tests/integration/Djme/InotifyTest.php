c8a65d06231bf9acf26df704a0b114bd2da66050
David JM Emmett <davidjmemmett@gmail.com>
2012-03-06 23:46:22 +0000

    added TODOs

--- tests/integration/Djme/InotifyTest.php
+++ tests/integration/Djme/InotifyTest.php
@@ -1,12 +1,12 @@
 <?php
 	
+	/**
+	 * @todo refactor tests
+	 */
 	class Djme_InotifyIntegrationTest extends PHPUnit_Framework_TestCase {
 	
 		/**
 		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
 		 * @todo add test_read_access
 		 */
 		
@@ -102,187 +102,47 @@
 		
 		/**
 		 * @todo add test_read_closeWriteWithoutTrigger
-		 */
-		 
-		/**
 		 * @todo add test_read_closeWrite
-		 */
-		
-		/**
 		 * @todo add test_read_closeNowriteWithoutTrigger
-		 */
-		 
-		/**
 		 * @todo add test_read_closeNowrite
-		 */
-		
-		/**
 		 * @todo add test_read_openWithoutTrigger
-		 */
-		 
-		/**
 		 * @todo add test_read_open
-		 */
-		
-		/**
 		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
 		 * @todo add test_read_access
-		 */
-		
-		const MOVED_TO = IN_MOVED_TO;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const MOVED_FROM = IN_MOVED_FROM;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const CREATE = IN_CREATE;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const DELETE = IN_DELETE;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const DELETE_SELF = IN_DELETE_SELF;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const MOVE_SELF = IN_MOVE_SELF;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const CLOSE = IN_CLOSE;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const MOVE = IN_MOVE;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const ALL_EVENTS = IN_ALL_EVENTS;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const UNMOUNT = IN_UNMOUNT;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const Q_OVERFLOW = IN_Q_OVERFLOW;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const IGNORED = IN_IGNORED;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const ISDIR = IN_ISDIR;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const ONLYDIR = IN_ONLYDIR;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const DONT_FOLLOW = IN_DONT_FOLLOW;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const MASK_ADD = IN_MASK_ADD;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
-		 */
-		
-		const ONESHOT = IN_ONESHOT;
-		/**
-		 * @todo add test_read_accessWithoutTrigger
-		 */
-		 
-		/**
-		 * @todo add test_read_access
+		 * @todo add test_read_movedToWithoutTrigger
+		 * @todo add test_read_movedTo
+		 * @todo add test_read_movedFromWithoutTrigger
+		 * @todo add test_read_movedFrom
+		 * @todo add test_read_createWithoutTrigger
+		 * @todo add test_read_create
+		 * @todo add test_read_deleteWithoutTrigger
+		 * @todo add test_read_delete
+		 * @todo add test_read_deleteSelfWithoutTrigger
+		 * @todo add test_read_deleteSelf
+		 * @todo add test_read_moveSelfWithoutTrigger
+		 * @todo add test_read_moveSelf
+		 * @todo add test_read_closeWithoutTrigger
+		 * @todo add test_read_close
+		 * @todo add test_read_moveWithoutTrigger
+		 * @todo add test_read_move
+		 * @todo add test_read_allEventsWithoutTrigger
+		 * @todo add test_read_allEvents
+		 * @todo add test_read_unmountWithoutTrigger
+		 * @todo add test_read_unmount
+		 * @todo add test_read_qOverflowWithoutTrigger
+		 * @todo add test_read_qOverflow
+		 * @todo add test_read_ignoredWithoutTrigger
+		 * @todo add test_read_ignored
+		 * @todo add test_read_isdirWithoutTrigger
+		 * @todo add test_read_isdir
+		 * @todo add test_read_onlydirWithoutTrigger
+		 * @todo add test_read_onlydir
+		 * @todo add test_read_dontFollowWithoutTrigger
+		 * @todo add test_read_dontFollow
+		 * @todo add test_read_maskAddWithoutTrigger
+		 * @todo add test_read_maskAdd
+		 * @todo add test_read_oneshotWithoutTrigger
+		 * @todo add test_read_oneshot
 		 */
 		
 	}
