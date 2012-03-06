c4e33260fcd62cdb3be96b65cb03312879540496
David JM Emmett <davidjmemmett@gmail.com>
2012-03-06 22:00:22 +0000

    ensured that temporary files are removed on tearDown

--- tests/unit/Djme/InotifyTest.php
+++ tests/unit/Djme/InotifyTest.php
@@ -2,6 +2,14 @@
 	
 	class Djme_InotifyTest extends PHPUnit_Framework_TestCase {
 		
+		private $temporaryFilepaths = array ();
+		
+		protected function tearDown() {
+			foreach ($this->temporaryFilepaths as $path) {
+				unlink($path);
+			}
+		}
+		
 		public function test_contructor_basic() {
 			$inotify = new Djme_Inotify();
 			$this->assertInstanceOf('Djme_Inotify', $inotify);
@@ -35,6 +43,7 @@
 		public function test_addWatch_withValidPathnameButNullMask() {
 			$inotify = new Djme_Inotify();
 			$pathname = tempnam(sys_get_temp_dir(), 'pre');
+			$this->temporaryFilepaths[] = $pathname;
 			$this->setExpectedException(
 				'Djme_Inotify_Exception_NullMask',
 				'Mask cannot be null.');
